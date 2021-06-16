8a76b9e8f82763a39ffcf91e91449d13134f33df
Mohamed Ragab <mohamedragab.cs@gmail.com>
2021-06-16 14:20:16 +0200

    first finish

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,73 @@
+/***************** side menu show / hide *****************/
+$(document).ready(function () {
+    if ($(window).width() <= 991) {
+        $(".main-navbar").addClass("slide-out");
+    }
+    $('.toggler-header button').click(function () {
+        $('.main-navbar').toggleClass('slide-in slide-out');
+        $('.fa-times').click(function () {
+            $('.main-navbar').addClass('slide-out').removeClass("slide-in");
+        })
+    })
+})
+
+/***************** navbar dropdowns show *****************/
+$(document).ready(function () {
+    $('.main-navbar .dropdown').click(function () {
+        alert('kksk')
+    });
+});
+
+
 /***************** speaker box on hover *****************/
 $(document).ready(function () {
     $('.speaker-box').hover(function () {
         $(this).find('.overlay-top').toggleClass('show');
     })
 })
+// slider of speakers
+$(document).ready(function () {
+    var getDirection = $('body').css('direction');
+    $('.speakers-slider').slick({
+        rows: 2,
+        autoplay: true,
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        rtl: getDirection === 'rtl' ? true : false,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
 
+    });
+});
+
+/***************** go top *****************/
+$(document).ready(function () {
+    var scrollTop = $('.go-top .fa-chevron-up');
+    $(scrollTop).click(function () {
+        $('html, body').animate({
+                scrollTop: 0,
+            },
+            1000
+        );
+        return false;
+    });
+});
